3778a82c9a4dcb6fb192789511603dd12adbbfd4
Di Wang <gxwangdi@gmail.com>
2017-09-17 18:24:01 -0700

    Implement error msg and loading state

--- Tumblr/Tumblr/PhotosViewController.swift
+++ Tumblr/Tumblr/PhotosViewController.swift
@@ -63,6 +63,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func initUI() {
+        
         refreshControl = UIRefreshControl();
         refreshControl.addTarget(self, action: #selector(PhotosViewController.handleRefresh(refreshControl:)), for: UIControlEvents.valueChanged);
         photoTableView.insertSubview(refreshControl, at: 0);
@@ -78,7 +79,9 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         let urlStr = "\(GlobalConstants.API_url)\(endpoint!)?api_key=\(GlobalConstants.API_KEY)";
         print(urlStr)
         let url = URL(string:urlStr);
-        let request = URLRequest(url: url!)
+        var request = URLRequest(url: url!)
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData;
+        
         let session = URLSession(
             configuration: URLSessionConfiguration.default,
             delegate:nil,
@@ -90,6 +93,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             completionHandler: { (data, response, error) in
                 
                 if error != nil {
+                    self.showToast(message: "Network Error.");
                     print("Network error.");
                     return;
                 }
@@ -106,6 +110,26 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 }
         });
         task.resume()
+    }
+    
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
     
     
